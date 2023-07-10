071b17ce8d76918e2eef41968f9eb956c1cce27d
Agustinus David Susanto <v-agustinus.susanto@anteraja.id>
2023-07-10 14:03:56 +0700

    add custom list and dashboard on custom production plan

--- catering_module/catering_module/doctype/custom_production_plan/custom_production_plan.py
+++ catering_module/catering_module/doctype/custom_production_plan/custom_production_plan.py
@@ -1,8 +1,125 @@
 # Copyright (c) 2023, Satu Meja and contributors
 # For license information, please see license.txt
 
-# import frappe
+import copy
+import json
+
+import frappe
+from frappe import _, msgprint
 from frappe.model.document import Document
+from frappe.query_builder.functions import IfNull, Sum
+from frappe.utils import (
+	add_days,
+	ceil,
+	cint,
+	comma_and,
+	flt,
+	get_link_to_form,
+	getdate,
+	now_datetime,
+	nowdate,
+)
+from frappe.utils.csvutils import build_csv_response
+from pypika.terms import ExistsCriterion
+
+from erpnext.manufacturing.doctype.bom.bom import get_children as get_bom_children
+from erpnext.manufacturing.doctype.bom.bom import validate_bom_no
+from erpnext.manufacturing.doctype.work_order.work_order import get_item_details
+from erpnext.setup.doctype.item_group.item_group import get_item_group_defaults
+from erpnext.stock.get_item_details import get_conversion_factor
+from erpnext.stock.utils import get_or_make_bin
+from erpnext.utilities.transaction_base import validate_uom_is_integer
 
 class CustomProductionPlan(Document):
 	pass
+
+	@frappe.whitelist()
+	def get_open_sales_orders(self):
+		"""Pull sales orders  which are pending to deliver based on criteria selected"""
+		open_so = get_sales_orders(self)
+
+		if open_so:
+			self.add_so_in_table(open_so)
+		else:
+			frappe.msgprint(_("Sales orders are not available for production"))
+
+	def add_so_in_table(self, open_so):
+		"""Add sales orders in the table"""
+		self.set("sales_orders", [])
+
+		for data in open_so:
+			self.append(
+				"sales_orders",
+				{
+					"sales_order": data.name,
+					"sales_order_date": data.transaction_date,
+					"customer": data.customer,
+					"grand_total": data.base_grand_total,
+				},
+			)
+
+def get_sales_orders(self):
+	bom = frappe.qb.DocType("BOM")
+	pi = frappe.qb.DocType("Packed Item")
+	so = frappe.qb.DocType("Sales Order")
+	so_item = frappe.qb.DocType("Sales Order Item")
+
+	open_so_subquery1 = frappe.qb.from_(bom).select(bom.name).where(bom.is_active == 1)
+
+	open_so_subquery2 = (
+		frappe.qb.from_(pi)
+		.select(pi.name)
+		.where(
+			(pi.parent == so.name)
+			& (pi.parent_item == so_item.item_code)
+			& (
+				ExistsCriterion(
+					frappe.qb.from_(bom).select(bom.name).where((bom.item == pi.item_code) & (bom.is_active == 1))
+				)
+			)
+		)
+	)
+
+	open_so_query = (
+		frappe.qb.from_(so)
+		.from_(so_item)
+		.select(so.name, so.transaction_date, so.customer, so.base_grand_total)
+		.distinct()
+		.where(
+			(so_item.parent == so.name)
+			& (so.docstatus == 1)
+			& (so.status.notin(["Stopped", "Closed"]))
+			& (so.company == self.company)
+			& (so_item.qty > so_item.work_order_qty)
+		)
+	)
+
+	date_field_mapper = {
+		"from_date": self.from_date >= so.transaction_date,
+		"to_date": self.to_date <= so.transaction_date,
+		"from_delivery_date": self.from_delivery_date >= so_item.delivery_date,
+		"to_delivery_date": self.to_delivery_date <= so_item.delivery_date,
+	}
+
+	for field, value in date_field_mapper.items():
+		if self.get(field):
+			open_so_query = open_so_query.where(value)
+
+	for field in ("customer", "project", "sales_order_status", "slot_pengiriman"):
+		if self.get(field):
+			so_field = "status" if field == "sales_order_status" else field
+			open_so_query = open_so_query.where(so[so_field] == self.get(field))
+
+	if self.item_code and frappe.db.exists("Item", self.item_code):
+		open_so_query = open_so_query.where(so_item.item_code == self.item_code)
+		open_so_subquery1 = open_so_subquery1.where(
+			self.get_bom_item_condition() or bom.item == so_item.item_code
+		)
+
+	open_so_query = open_so_query.where(
+		(ExistsCriterion(open_so_subquery1) | ExistsCriterion(open_so_subquery2))
+	)
+
+	open_so = open_so_query.run(as_dict=True)
+
+	return open_so
