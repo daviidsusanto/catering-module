d6f0f420f7698905143c23af8fe57e0e8abd383c
Agustinus David Susanto <v-agustinus.susanto@anteraja.id>
2023-07-10 12:53:38 +0700

    initial commit with seperate completed task

--- catering_module/hooks.py
+++ catering_module/hooks.py
@@ -30,6 +30,10 @@ app_license = "MIT"
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {
+    "BOM" : "public/js/bom_addon.js",
+    "BOM Item" : "public/js/bom_addon.js"
+    }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
---
+++ catering_module/public/js/bom_addon.js
@@ -0,0 +1,21 @@
+frappe.ui.form.on('BOM Item', {
+	qty_recipe: function(frm,cdt,cdn) {
+		var child = locals[cdt][cdn];
+        if(child.item_code){
+            frappe.call({
+                method: 'frappe.client.get_value',
+                args: {
+                  doctype: 'Item',
+                  filters: { name: child.item_code },
+                  fieldname: 'q_factor'
+                },
+                callback: function(response) {
+                    var data = response.message;
+                    var q_factor = data.q_factor;
+                    var q_req = child.qty_recipe * (1 + q_factor);
+                    frappe.model.set_value(cdt, cdn, "qty", q_req);
+                }
+              });
+        }
+	},
+});
