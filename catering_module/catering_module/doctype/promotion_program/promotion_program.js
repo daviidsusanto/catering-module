0c275413b3a3e4765a04c2efd8376c103ef52b5c
Agustinus David Susanto <v-agustinus.susanto@anteraja.id>
2023-08-14 08:59:40 +0700

    initial for phase 2 module, update doctype and add endpoint nearest dist point

---
+++ catering_module/catering_module/doctype/promotion_program/promotion_program.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Satu Meja and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Promotion Program', {
+	// refresh: function(frm) {
+
+	// }
+});
--- catering_module/public/api.py
+++ catering_module/public/api.py
@@ -1,4 +1,7 @@
 import frappe 
+import requests
+import random
+import string
 
 @frappe.whitelist()
 def list_work_order(tgl_pengiriman, slot_pengiriman):
@@ -24,3 +27,61 @@ def list_work_order(tgl_pengiriman, slot_pengiriman):
             })
 
     frappe.response['data'] = output
+
+@frappe.whitelist()
+def get_nearest_distribution_point(my_location):
+    try:
+        base_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+
+        distribution_points = frappe.get_list("Distribution Point",fields=["name","latlong","address"])
+        
+        origins = my_location
+        destinations = '|'.join(point['latlong'] for point in distribution_points)
+
+        params = {
+            "origins": origins,
+            "destinations": destinations,
+            "key": frappe.db.get_single_value("Google API Setup","api_key")
+        }
+
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if data.get("status") == "OK":
+            elements = data["rows"][0]["elements"]
+            min_distance = float('inf')
+            nearest_point_index = -1
+
+            for i, element in enumerate(elements):
+                if element.get("status") == "OK":
+                    distance = element["distance"]["value"]
+                    if distance < min_distance:
+                        min_distance = distance
+                        nearest_point_index = i
+            nearest_point = []
+            if nearest_point_index != -1:
+                nearest_point.append(distribution_points[nearest_point_index])
+                frappe.response["code"] = 200
+                frappe.response["message"] = "Success"
+                frappe.response["data"] = nearest_point
+        else:
+            frappe.response["code"] = 400
+            frappe.response["message"] = "Request Failed"
+            frappe.response["data"] = data
+            
+    except Exception as e:
+        frappe.response["code"] = 400
+        frappe.response["message"] = "Request Failed"
+        frappe.response["data"] = e
+
+@frappe.whitelist()
+def generate_unique_strings(num_strings, length):
+    generated_strings = set()
+    while len(generated_strings) < num_strings:
+        alphanumeric_characters = string.ascii_letters + string.digits
+        new_string = ''.join(random.choice(alphanumeric_characters) for _ in range(length)).upper()
+        if new_string not in generated_strings:
+            generated_strings.add(new_string)
+            yield new_string
+
+    return generated_strings
