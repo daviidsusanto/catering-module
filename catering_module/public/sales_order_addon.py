cbdaef8282c8985c17b719418e6f8cd9e5c427a3
Agustinus David Susanto <v-agustinus.susanto@anteraja.id>
2023-08-17 14:31:10 +0700

    generate barcode on so

---
+++ catering_module/public/sales_order_addon.py
@@ -0,0 +1,24 @@
+import frappe
+from frappe.model.document import Document
+import json
+import math
+import random
+import string
+
+def generate_barcode_so(doc,name):
+    if doc.total_qty > 0:
+        length = 4
+        num_strings = math.ceil(int(doc.total_qty) / 10)
+        generated_strings = set()
+        for i in range(0,num_strings):
+            alphanumeric_characters = string.ascii_letters + string.digits
+            new_string = ''.join(random.choice(alphanumeric_characters) for _ in range(length)).upper()
+            if new_string not in generated_strings:
+                generated_strings.add(new_string)
+                
+        if generated_strings:
+            doc.barcode = []
+            for i in generated_strings:
+                doc.append("barcode",{
+                    "barcode": doc.name + "-" + i
+                })
