8c4b0f54cf7777fa333df9aeba035a2631738f23
Agustinus David Susanto <v-agustinus.susanto@anteraja.id>
2023-09-07 16:17:47 +0700

    update field on sales order and barcode and add callback api

--- catering_module/public/sales_order_addon.py
+++ catering_module/public/sales_order_addon.py
@@ -4,13 +4,31 @@ import json
 import math
 import random
 import string
+from itertools import groupby
 
 def generate_barcode_so(doc,name):
     if doc.total_qty > 0:
         length = 4
         num_strings = math.ceil(int(doc.total_qty) / 10)
         generated_strings = set()
-        for i in range(0,num_strings):
+
+        data = []
+        for j in doc.items:
+            data.append({'qty': j.qty,'item_category': j.item_category})
+
+        sorted_data = sorted(data, key=lambda x: x["item_category"])
+
+        result = {
+                    key: sum(item["qty"] for item in group)
+                    for key, group in groupby(sorted_data, key=lambda x: x["item_category"])
+                }
+        mb = 0
+        for x in result:
+            value = result[x]
+            qty_per_master_box = frappe.get_value("Catering Masterbox",{'name': x},'quantity_per_master_box')
+            mb += math.ceil(value / qty_per_master_box)
+
+        for i in range(0,mb):
             alphanumeric_characters = string.ascii_letters + string.digits
             new_string = ''.join(random.choice(alphanumeric_characters) for _ in range(length)).upper()
             if new_string not in generated_strings:
