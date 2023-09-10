f59b40e8c9ef8c743de86aad3bf7198c18457ea9
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2023-09-10 23:34:46 +0700

    feat: add qz pkce signing api

---
+++ catering_module/public/qz_signing.py
@@ -0,0 +1,20 @@
+import frappe
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+
+@frappe.whitelist()
+def sign_message():
+    message = frappe.request.args['message']
+    # Load signature
+    b64_private_key = os.environ['QZ_PRIVATE_KEY']
+    private_key = base64.b64decode(b64_private_key)
+    key = serialization.load_pem_private_key(private_key, None, backend=default_backend())
+    # Create the signature
+    signature = key.sign(message.encode('utf-8'), padding.PKCS1v15(), hashes.SHA512())  # Use hashes.SHA1() for QZ Tray 2.0 and older
+    # Echo the signature
+    frappe.response.display_content_as="inline"
+    frappe.response.type="text"
+    frappe.response.result=signature
