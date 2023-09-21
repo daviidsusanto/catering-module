7a21466ecae5b122d79fbd4ffd9e8467060e9b2e
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2023-09-21 23:58:55 +0700

    chore: fix qz api response for ajax call

--- catering_module/public/qz_signing.py
+++ catering_module/public/qz_signing.py
@@ -15,10 +15,10 @@ def sign_message():
     # Create the signature
     signature = key.sign(message.encode('utf-8'), padding.PKCS1v15(), hashes.SHA512())  # Use hashes.SHA1() for QZ Tray 2.0 and older
     # Echo the signature
-    frappe.response.type="txt"
+    frappe.response.type="download"
+    frappe.response.content_type="text/plain"
     frappe.response.display_content_as="inline"
-    frappe.response.doctype="signature"
-    frappe.response.result=base64.b64encode(signature).decode("ascii")
+    frappe.response.filecontent=base64.b64encode(signature).decode("ascii")
 
 @frappe.whitelist()
 def qz_certificate():
@@ -26,7 +26,7 @@ def qz_certificate():
     cert = base64.b64decode(b64_private_key)
     # Create the signature
     # Echo the signature
-    frappe.response.type="txt"
+    frappe.response.type="download"
+    frappe.response.content_type="text/plain"
     frappe.response.display_content_as="inline"
-    frappe.response.result=cert
-    frappe.response.doctype="certificate"
+    frappe.response.filecontent=cert
