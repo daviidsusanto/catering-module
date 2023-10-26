aecac39abb743a8aaeaf2b0ac1f02483d496e8b7
Agustinus David Susanto <v-agustinus.susanto@anteraja.id>
2023-10-26 12:00:31 +0700

    update

--- catering_module/public/js/sales_order_addon.js
+++ catering_module/public/js/sales_order_addon.js
@@ -1,30 +1,3 @@
-// frappe.ui.form.on('Sales Order', {
-// 	validate: function(frm) {
-// 		frappe.call({
-//             method: "frappe.www.printview.get_rendered_raw_commands",
-//             args: {
-//                 doc: frm.doc,
-//                 print_format: "Sales Order Raw Printing",
-//             },
-//             callback: function (r) {
-//                 if (!r.exc) {
-//                     let data = [r.message.raw_commands];
-//                     frappe.ui.form.qz_connect()
-//                     .then(function () {
-//                         let config = qz.configs.create("Nama Printer");
-//                         console.log(config)
-//                         return qz.print(config, data);
-//                     })
-//                     .then(frappe.ui.form.qz_success)
-//                     .catch(err => {
-//                         frappe.ui.form.qz_fail(err);
-//                     });
-//                 }
-//             },
-//         });
-// 	},
-// });
-
 frappe.ui.form.on('Sales Order', {
 	on_submit: function(frm) {
 	  frappe.call({
