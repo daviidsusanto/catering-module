b554c93fce2d2ddc45ca58d7d6515c4e50ef02a8
Sandy Satriya <sandy@multiinti.com>
2023-12-10 20:26:26 +0700

    added new doctype

---
+++ catering_module/catering_module/doctype/record_jam_pengiriman/record_jam_pengiriman.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Satu Meja and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Record Jam Pengiriman', {
+	// refresh: function(frm) {
+
+	// }
+});
