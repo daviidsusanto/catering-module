bbb20b6d3dd2bb1800577b2a4c7f359e5fa2eb0c
daviidsusanto <daviidsusanto@gmail.com>
2024-01-02 16:23:25 +0700

    add doctype CUstom Production Sales Order

---
+++ catering_module/catering_module/doctype/custom_production_plan_sales_order/custom_production_plan_sales_order.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Satu Meja and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class CustomProductionPlanSalesOrder(Document):
+	pass
